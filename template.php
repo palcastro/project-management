1d9ed1b6fc67374bb8df8f1af18fde377bc74554
paloma castro <pcaformacion@gmail.com>
2022-01-19 08:52:03 +0100

    first attempt

---
+++ template.php
@@ -0,0 +1,20 @@
+<?php
+use phpGrid\C_DataGrid;
+require_once("../phpGrid/conf.php");      
+include_once('../inc/head.php');
+?>
+
+<h1>My Custom CRM</h1>
+<!-- Llama a la BBDD -->
+<?php
+include_once('../inc/menu.php');
+?>
+<!-- Donde se realizan los datos de consulta -->
+<h3>Section Title</h2>
+<?php
+// ..phpGrid code goes here
+?>
+
+<?php
+include_once('../inc/footer.php');
+?>
